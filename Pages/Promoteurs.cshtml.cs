be67cbcb3b1f6504d95e6eb648dd8004a3c7aaef
Soufiane El Kassib <poliamus@yahoo.com>
2019-05-26 05:37:27 +0000

    Ajouter des liens vers la methode create

---
+++ Pages/Promoteurs.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ASimmo.Data;
+using ASimmo.Models;
+
+namespace ASimmo.Pages
+{
+    public class PromoteursModel : PageModel
+    {
+        private readonly ASimmo.Data.ApplicationDbContext _context;
+
+        public PromoteursModel(ASimmo.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Promoteur> Promoteur { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Promoteur = await _context.Promoteurs
+                .Include(p => p.Type)
+                .Include(p => p.User).ToListAsync();
+        }
+    }
+}
